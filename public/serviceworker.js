5f17c23709c9f2eedc967dfa761834052c2da21e
James O'Keefe <jbo88@att.net>
2021-08-21 00:27:00 -0400

    minor adjustments

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,12 +2,12 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/index.js',
+    '/db.js',
     '/styles.css',
     '/manifest.webmanifest',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     'webpack.config.js',
-    '/db.js',
   ];
   
   const PRECACHE = 'precache-v1';
